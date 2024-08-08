55695629a060fa965ceea7e3f0fadb1eaba2224d
Craig West <iwswordpress@gmail.com>
2024-08-08 10:48:23 +0100

    edit logging tests

--- tests/00_setup/test_02_config.py
+++ tests/00_setup/test_02_config.py
@@ -8,7 +8,7 @@ from utils.read_config import get_version
 VERSION = "1.0.0"
 
 
-def test_900_get_version():
+def test_GEN_001_get_version():
     """Test get_version returns correct version"""
     version = get_version()
     assert version == VERSION
--- tests/00_setup/test_03_logging.py
+++ tests/00_setup/test_03_logging.py
@@ -9,19 +9,19 @@ import pytest
 LOGGER = logging.getLogger(__name__)
 
 
-def test_0008_a1():
-    """A test"""
-    LOGGER.info("test_a1")
+def test_GEN_002_log_info():
+    """testing log info"""
+    LOGGER.info("testing log info")
     assert 4 != 3
 
 
-def test_0009_a2():
-    """A test"""
-    LOGGER.warn("test_a2")
+def test_GEN_003_log_warn():
+    """testing log warn"""
+    LOGGER.warn("testing log warn")
     assert 1
 
 
-def test_0001_a3():
-    """A test"""
-    LOGGER.critical("critical test 0001")
+def test_GEN_004_log_critical():
+    """testing log critical"""
+    LOGGER.critical("testing log critical")
     assert 1
